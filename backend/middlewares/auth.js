3ca7f29167aaf97815990c3e3d74e99bffe7a10d
Gregory-mcbit <76042617+Gregory-mcbit@users.noreply.github.com>
2022-06-12 18:39:46 +0300

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,8 +2,6 @@ const jwt = require('jsonwebtoken');
 
 const AuthError = require('../errors/auth_error');
 
-const JWT_SECRET = 'secret';
-
 function auth(req, res, next) {
   const token = req.cookies.jwt;
   let payload;
